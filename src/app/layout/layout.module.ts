2c06ef231c2d699d7f44d6024f8275912c171522
Santosh Kumar <santosh.k@technovert.com>
2023-02-20 22:36:02 +0530

    layout added

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
   imports: [
     CommonModule,
     LayoutRoutingModule
--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -1,9 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
+  constructor(
+    public http: HttpClient
+  ) { }
 
-  constructor() { }
+  get(url: string) {
+    return this.http.get(url) as Observable<any>;
+  }
+
+  getAll(url: string) {
+    return this.http.get(url) as Observable<any[]>;
+  }
+
+  post(url: string, model: any) {
+    return this.http.post(url, model) as Observable<any>;
+  }
+
+  put(url: string, model: any) {
+    return this.http.put(url, model);
+  }
+
+  delete(url: string) {
+    return this.http.delete(url);
+  }
+
+  upload(url: string, model: any) {
+    const formData: FormData = new FormData();
+    for (const key in model) {
+      if (model.hasOwnProperty(key)) {
+        formData.append(key, model[key]);
+      }
+    }
+    return this.http.post(url, formData);
+  }
+
+  getWithOptions(url: string, options: any) {
+    return this.http.get(url, options) as Observable<any>;
+  }
+
+  postWithOptions(url: string, model: any, options: any) {
+    return this.http.post(url, model, options) as Observable<any>;
+  }
+
 }
